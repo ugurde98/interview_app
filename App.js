43d71c3ce50164d438b004a72e7bd36f1838266b
ugurde98 <ugurde98@gmail.com>
2023-04-30 01:30:27 +0300

    screens created

--- App.js
+++ App.js
@@ -3,50 +3,9 @@ import { Alert, Button, StyleSheet, Text, View } from "react-native";
 import { useAuth0, Auth0Provider } from "react-native-auth0";
 import { Provider as PaperProvider } from 'react-native-paper';
 import { useTheme } from 'react-native-paper';
+import AppNavigator from "./navigation/AppNavigator";
 
 
-const Home = () => {
-  const { authorize, clearSession, user, error, getCredentials } = useAuth0();
-
-  const onLogin = async () => {
-    try {
-      await authorize(
-        { scope: "openid profile email" },
-        { customScheme: "interview" }
-      );
-      let credentials = await getCredentials();
-      Alert.alert("AccessToken: " + credentials.accessToken);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  const loggedIn = user !== undefined && user !== null;
-
-  const onLogout = async () => {
-    try {
-      await clearSession({ customScheme: "interview" });
-    } catch (e) {
-      console.log("Log out cancelled");
-    }
-  };
-  const theme = useTheme();
-
-
-  return (
-    <View style={styles.container}>
-      <Text style={{ color: theme.colors.primary }}> Auth0Sample - Login </Text>
-      {user && <Text>You are logged in as {user.name}</Text>}
-      {!user && <Text>You are not logged in</Text>}
-      {error && <Text>{error.message}</Text>}
-      <Button
-      
-        onPress={loggedIn ? onLogout : onLogin}
-        title={loggedIn ? "Log Out" : "Log In"}
-      />
-    </View>
-  );
-};
 
 const App = () => {
   return (
@@ -55,24 +14,14 @@ const App = () => {
       clientId={"3M1YSfCUV11jWKvGmncL3Lf0MMh34UL5"}
     >
     <PaperProvider>
-      <Home />
+      <AppNavigator/>
     </PaperProvider>
     </Auth0Provider>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF",
-  },
-  header: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10,
-  },
+  
 });
 
 export default App;
---
+++ navigation/AppNavigator.js
@@ -0,0 +1,43 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import HomeScreen from "../screens/HomeScreen";
+import UserScreen from "../screens/UserScreen";
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+const HomeStackNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen}  options={{headerShown:false}}/>
+    </Stack.Navigator>
+  );
+};
+
+const UserStackNavigator = () => {
+    return (
+      <Stack.Navigator>
+        <Stack.Screen name="User" component={UserScreen} options={{headerShown:false}} />
+      </Stack.Navigator>
+    );
+  };
+const TabNavigator = () => {
+    return(
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeStackNavigator} />
+      <Tab.Screen name="Settings" component={UserStackNavigator} />
+    </Tab.Navigator> 
+    )
+};
+const AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <TabNavigator />
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigator;
