51d92e6a4903163c59dff848aa46c3512d53dec5
ugurde98 <ugurde98@gmail.com>
2023-04-30 05:46:14 +0300

    add apiConfig and example use marvel api

---
+++ apiConfig.js
@@ -0,0 +1,7 @@
+export const apiConfig  = {
+ 
+    apiUrl :'https://gateway.marvel.com:443/',
+    hash:"245956ea68465d438b3a5e4139c30071",
+    apiKey:"26f813c06e6314419278d91f7d38177b",
+    ts:"1",
+  };
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,28 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from 'axios';
+import { apiConfig } from '../apiConfig';
 
 const HomeScreen = () => {
+    const service=() => {
+        let config = {
+            method: 'get',
+            maxBodyLength: Infinity,
+            url: `${apiConfig.apiUrl}v1/public/comics?apikey=${apiConfig.apiKey}&hash=${apiConfig.hash}&ts=${apiConfig.ts}&limit=1`,
+            headers: { }
+          };
+          axios.request(config)
+          .then((response) => {
+            console.log(JSON.stringify(response.data));
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+    }
+    useEffect(() => {
+        service()
+    }, [])
+    
   return (
     <View>
       <Text>HomeScreen</Text>
