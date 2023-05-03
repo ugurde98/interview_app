ac0118320a50f6592edb4e429f7aa1295d76e716
ugurde98 <ugurde98@gmail.com>
2023-05-03 21:07:51 +0300

    Header Added

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -2,37 +2,68 @@ import { View, Text } from "react-native";
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import HomeScreen from "../screens/HomeScreen";
 import UserScreen from "../screens/UserScreen";
 import ComicDetailsScreen from "../screens/ComicDetailsScreen";
+import MarvelLogo from "../assets/MarvelLogo";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 const HomeStackNavigator = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="HomeStack" component={HomeScreen}  options={{headerShown:false}}/>
-      <Stack.Screen name="ComicDetails" component={ComicDetailsScreen}  options={{headerShown:false}}/>
+      <Stack.Screen
+        name="HomeStack"
+        component={HomeScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="ComicDetails"
+        component={ComicDetailsScreen}
+        options={{ headerShown: false }}
+      />
     </Stack.Navigator>
   );
 };
 
 const UserStackNavigator = () => {
-    return (
-      <Stack.Navigator>
-        <Stack.Screen name="User" component={UserScreen} options={{headerShown:false}} />
-      </Stack.Navigator>
-    );
-  };
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="User"
+        component={UserScreen}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
+  );
+};
 const TabNavigator = () => {
-    return(
+  return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeStackNavigator} />
-      <Tab.Screen name="Settings" component={UserStackNavigator} />
-    </Tab.Navigator> 
-    )
+      <Tab.Screen
+        name="Home"
+        component={HomeStackNavigator}
+        options={{
+          headerTitle: () => <MarvelLogo />,
+          headerStyle:{
+            backgroundColor:"#EC1D24",
+          }
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={UserStackNavigator}
+        options={{
+          headerTitle: () => <MarvelLogo />,
+          headerStyle:{
+            backgroundColor:"#EC1D24",
+          }
+        }}
+      />
+    </Tab.Navigator>
+  );
 };
 const AppNavigator = () => {
   return (
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -52,6 +52,7 @@ const styles = StyleSheet.create({
   container:{
     flex:1,
     justifyContent:'center',
-    alignContent:'center'
+    alignContent:'center',
+    marginTop:10
   }
 });
