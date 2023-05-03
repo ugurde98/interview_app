cf16fd3162bbc1cbcc8767f30e1c02ff37041fd8
ugurde98 <ugurde98@gmail.com>
2023-05-03 21:58:42 +0300

    Comic Details Api Service Connected and Data transfer to state

--- screens/ComicDetailsScreen.js
+++ screens/ComicDetailsScreen.js
@@ -1,16 +1,48 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { apiConfig } from "../apiConfig";
+import axios from "axios";
 
 const ComicDetailsScreen = (props) => {
-    const {itemId}=props.route.params
-    console.log('itemId: ', itemId);
+  const { itemId } = props.route.params;
+  console.log('itemId: ', itemId);
+
+  const [data, setData] = useState(null);
+  console.log('data: ', data);
+
+  const service = () => {
+    let config = {
+      method: "get",
+      maxBodyLength: Infinity,
+      url: `${apiConfig.apiUrl}v1/public/comics/${itemId}?apikey=${apiConfig.apiKey}&hash=${apiConfig.hash}&ts=${apiConfig.ts}&limit=10`,
+      headers: {},
+    };
+    axios
+      .request(config)
+      .then((response) => {
+        setData(response.data.data.results[0]);
+      })
+      .catch((error) => {});
+  };
+  useEffect(() => {
+    service();
+  }, []);
   return (
-    <View>
+    <View style={styles.container}>
       <Text>ComicDetailsScreen</Text>
+      {data ?<Text>{data.title}</Text> :<Text>asda</Text>}
+
     </View>
-  )
-}
+  );
+};
 
-export default ComicDetailsScreen
+export default ComicDetailsScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    margin: 20,
+  },
+});
